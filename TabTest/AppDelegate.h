53b462370d36d516d00a6327961e6ca97139690c
Joshua Luongo <josh@jrapps.com.au>
2015-11-04 23:12:43 +1100

    Uploaded sample code.

---
+++ TabTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TabTest
+//
+//  Created by Joshua Luongo on 4/11/2015.
+//  Copyright © 2015 JR Apps. All rights reserved.
+//
+
+@import UIKit;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TabTest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TabTest
+//
+//  Created by Joshua Luongo on 4/11/2015.
+//  Copyright © 2015 JR Apps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
